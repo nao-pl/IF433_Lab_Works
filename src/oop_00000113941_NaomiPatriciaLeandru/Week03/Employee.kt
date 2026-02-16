9f439a0f61f826d233ad87801e48519065018685
Nao <naomileandru@gmail.com>
2026-02-16 15:09:35 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000113941_NaomiPatriciaLeandru/Week03/Employee.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week03/Employee.kt
@@ -1,5 +1,9 @@
 package oop_00000113941_NaomiPatriciaLeandru.Week03
 
-class Employee(val name: string){
+class Employee(val name: String){
     var salary: Int = 0
+        set(value){
+            println("Mencoba set gaji ke: $value")  
+            this.salary=value      
+        }
 }
---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week03
+
+fun main(){
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
