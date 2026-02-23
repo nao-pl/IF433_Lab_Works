c16b6b5b681e8133f0fd9693a78ba0cb04c0dffd
Nao <naomileandru@gmail.com>
2026-02-23 12:39:50 +0700

    week04: implement super keyword in Car accelerate

--- src/oop_00000113941_NaomiPatriciaLeandru/Week04/Vehicle.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week04/Vehicle.kt
@@ -21,4 +21,9 @@ open class Car(brand: String, val numberOfDoors: Int) : vehicle(brand){
     override fun honk(){
         println("TIN TIN! Mobil $brand lewat!")
     }
+
+    override fun accelerate(){
+        super.accelerate()
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
+    }
 }
