6bb1f63354b1c5ca869887e286b031140baf0788
Nao <naomileandru@gmail.com>
2026-03-02 15:24:29 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000113941_NaomiPatriciaLeandru/Week05/MainPaymentMethod.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week05/MainPaymentMethod.kt
@@ -1,12 +1,19 @@
 package oop_00000113941_NaomiPatriciaLeandru.Week05
 
-fun main(){
-    val EWallet = EWallet("Layla", 50000.0)
+fun main() {
+
+    val eWallet = EWallet("Layla", 50000.0)
     val creditCard = CreditCard("Layla", 100000.0)
 
-    val paymentAmount: List<PaymentMethod> = listOf(EWallet, creditCard)
+    val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
 
-    for(payment in paymentAmount){
+    for (payment in paymentMethods) {
+
         payment.processPayment(75000.0)
+
+        if (payment is EWallet) {
+            payment.topUp(50000.0)       
+            payment.processPayment(75000.0) 
+        }
     }
 }
