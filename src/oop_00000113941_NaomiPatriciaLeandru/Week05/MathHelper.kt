9c1db944fab338aef835703a6793f4bd8cd5bb80
Nao <naomileandru@gmail.com>
2026-03-02 14:57:55 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week05
+
+fun hitungLuas(sisi: Int): Int{
+    return sisi * sisi 
+}
+
+fun hitungLuas(panjang: Int, lebar: Int): Int{
+    return panjang * lebar
+}
+
+fun hitungLuas(jariJari: Double): Double{
+    return 3.14 * jariJari * jariJari
+}
