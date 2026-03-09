5007da89dfd013c9a23855f3d0a0f15b956a1580
Nao <naomileandru@gmail.com>
2026-03-09 12:49:03 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week06/MainSmartInterfaces.kt
@@ -0,0 +1,7 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week06
+
+fun main(){
+    val smartLamp = SmartLamp("1", "Ruang Tamu")
+    val smartSpeaker = SmartSpeaker("2", "Google Nest Dapur")
+    val smartCCTV = SmartCCTV("3", "Esviz Garasi")
+}
