447e928badda336e2e0ad13a7286b8b297790d92
Nao <naomileandru@gmail.com>
2026-03-09 11:37:51 +0700

    week06: create Button class implementing Clickable

--- src/oop_00000113941_NaomiPatriciaLeandru/Week06/Clickable.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week06/Clickable.kt
@@ -1,6 +1,12 @@
 package oop_00000113941_NaomiPatriciaLeandru.Week06
 
-interface Cliackable{
+interface Clickable{
     val name: String
     fun click()
+}
+
+class Button(override val name: String) : Clickable{
+    override fun click(){
+        println("Tombol '$name' berhasil diklik!")
+    }
 }
