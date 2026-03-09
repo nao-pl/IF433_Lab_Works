3056249fffd422ca30f9e9b6475dad95470f8386
Nao <naomileandru@gmail.com>
2026-03-09 11:50:27 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week06
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
