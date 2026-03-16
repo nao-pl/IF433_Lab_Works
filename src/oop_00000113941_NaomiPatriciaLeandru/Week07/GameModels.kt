f4fb7e1811ac22fc2bb79decb3654d9b0f454787
Nao <naomileandru@gmail.com>
2026-03-16 12:08:23 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week07/GameModels.kt
@@ -0,0 +1,15 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week07
+
+enum class ItemRarity {
+    COMMON, UNCOMMON, RARE, EPIC, LEGENDARY;
+
+    val dropChance: Int
+        get() = when(this){
+            COMMON -> 50
+            UNCOMMON -> 30
+            RARE -> 15
+            EPIC -> 4
+            LEGENDARY -> 1
+        }
+    }
+
