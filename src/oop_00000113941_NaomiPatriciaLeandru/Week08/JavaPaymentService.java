3cbf509ef431ab3782a170d2b31386d34a176981
Nao <naomileandru@gmail.com>
2026-04-13 14:05:30 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week08;
+
+public class JavaPaymentService{
+    public static String processPayment(String productId){
+        return "TRX-" + productID + "-SUCCESS";
+    }
+}
