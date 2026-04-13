2128ddaf21836a0f3326bca1f235ce13047d75c2
Nao <naomileandru@gmail.com>
2026-04-13 13:09:12 +0700

    week08: implement chained safe calls and elvis

--- src/oop_00000113941_NaomiPatriciaLeandru/Week08/OrderModels.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week08/OrderModels.kt
@@ -3,4 +3,4 @@ package oop_00000113941_NaomiPatriciaLeandru.Week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class Order(val delliveryDetails: DeliveryDetails?, val totalPrice: Int?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
