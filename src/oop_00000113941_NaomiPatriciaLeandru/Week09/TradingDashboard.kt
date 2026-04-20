07b9c919117637b1063800d1a0b5d3fa1f4ff118
Nao <naomileandru@gmail.com>
2026-04-20 14:55:31 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000113941_NaomiPatriciaLeandru/Week09/TradingDashboard.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week09/TradingDashboard.kt
@@ -24,4 +24,8 @@ fun main(){
         .map { "LOSS [${it.pair} - ${it.position.uppercase()}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
     val uniquePairs = tradeHistory.map { it.pair }.toSet()
+
+    println("\n=== CRYPTO TRADING DASHBOARD ===")
+    println("Unique Pairs Traded: ${uniquePairs.joinToString(", ")}")
+    topPerformerString.forEach { println(it) }
 }
