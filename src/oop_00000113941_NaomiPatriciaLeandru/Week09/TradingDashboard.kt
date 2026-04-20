fcef40dd127e6b55282ea17c10d351316ee85f15
Nao <naomileandru@gmail.com>
2026-04-20 13:57:49 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_00000113941_NaomiPatriciaLeandru/Week09/TradingDashboard.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week09/TradingDashboard.kt
@@ -14,4 +14,8 @@ fun main(){
     val closedTrades = tradeHistory.filter { it.status == "Closed" }
     val winningTrades = closedTrades.filter { it.roe > 0 }
     val losingTrades = closedTrades.filter { it.roe <= 0 }
+
+    val topPerformerString = winningTrades
+        .sortedByDescending { it.roe }
+        .map { "WIN [${it.pair} - ${it.position.uppercase()}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
 }
