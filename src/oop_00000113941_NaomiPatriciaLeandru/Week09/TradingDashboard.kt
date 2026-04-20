fb66323b987794559bf20f176ab50ae31e70cbce
Nao <naomileandru@gmail.com>
2026-04-20 13:41:47 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTC/USDT", "Long", 10, 15.5, "Closed"),
+        TradeLog("ETH/USDT", "Short", 5, -8.2, "Closed"),
+        TradeLog("XRP/USDT", "Long", 20, 25.0, "Open"),
+        TradeLog("ADA/USDT", "Short", 15, -12.3, "Closed"),
+        TradeLog("SOL/USDT", "Long", 8, 10.0, "Closed"),
+        TradeLog("DOT/USDT", "Short", 12, -5.0, "Closed")
+    )
+}
