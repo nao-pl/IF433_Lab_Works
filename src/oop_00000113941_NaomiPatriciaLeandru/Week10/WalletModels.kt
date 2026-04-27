d1cc9242f81ab76e16a3b8e18a716eaf431a0aad
Nao <naomileandru@gmail.com>
2026-04-27 12:45:49 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week10
+
+data class Coin(
+    val name: String, 
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
