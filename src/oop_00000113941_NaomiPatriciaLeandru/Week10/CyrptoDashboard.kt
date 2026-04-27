caa03ca3a6633d7a52dba1fd914e172a0091279a
Nao <naomileandru@gmail.com>
2026-04-27 13:01:43 +0700

    week10: (task) populate coin repository

--- src/oop_00000113941_NaomiPatriciaLeandru/Week10/CyrptoDashboard.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week10/CyrptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000113941_NaomiPatriciaLeandru.Week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.addItem(Coin("BTC", 2.5))
+    coinRepo.addItem(Coin("ETH", 5.0))
+    coinRepo.addItem(Coin("USDT", 10.0))
 }
