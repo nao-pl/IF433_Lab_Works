2263c9282fb31a439ff95f60312be9ba3b622500
Nao <naomileandru@gmail.com>
2026-04-27 12:37:52 +0700

    week10: implement generic function with return type

--- src/oop_00000113941_NaomiPatriciaLeandru/Week10/GenericMethods.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week10/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000113941_NaomiPatriciaLeandru.Week10
 fun <T> printData(data: T){
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T{
+    return input
+}
