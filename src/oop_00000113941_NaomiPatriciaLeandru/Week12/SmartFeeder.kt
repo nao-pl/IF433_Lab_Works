418df5df873d56034b978f0edb775afaa2568640
Nao <naomileandru@gmail.com>
2026-05-11 20:12:09 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_00000113941_NaomiPatriciaLeandru/Week12/SmartFeeder.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week12/SmartFeeder.kt
@@ -35,5 +35,7 @@ fun main(){
         println("${e.message}")
     }catch(e: Exception){
         println("${e.message}")
+    }finally{
+        println("Siklus pengecekan dispenser pagi selesai")
     }
 }
