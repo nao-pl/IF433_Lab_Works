128c9022654c8406b5201f8c34ab4bcd9bc76958
Nao <naomileandru@gmail.com>
2026-05-18 19:47:37 +0700

    "week13: create Student model"

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week13/StudentSerialization.kt
@@ -0,0 +1,9 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String, 
+    val age: Int, 
+    val gpa: Double
+)
