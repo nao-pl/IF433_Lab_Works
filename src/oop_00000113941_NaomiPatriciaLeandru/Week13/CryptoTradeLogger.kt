8b119faa7e9f466b3c2273b0b3f644b6356a4566
Nao <naomileandru@gmail.com>
2026-05-18 19:54:33 +0700

    "week13: (task) create TradeRecord data class"

---
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week13/CryptoTradeLogger.kt
@@ -0,0 +1,10 @@
+package oop_00000113941_NaomiPatriciaLeandru.Week13
+import java.io.File
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String, 
+    val type: String, 
+    val margin: Double, 
+    val pnl: Double
+)
