58f25ee961a2cd3011d57ffdb5dc5d2e905f46f6
Nao <naomileandru@gmail.com>
2026-05-18 19:56:48 +0700

    "week13: (task) implement fromCsvTrade with split"

--- src/oop_00000113941_NaomiPatriciaLeandru/Week13/CryptoTradeLogger.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week13/CryptoTradeLogger.kt
@@ -10,3 +10,10 @@ data class TradeRecord(
 )
 
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
+
+fun fromCsvTrade(line: String): TradeRecord?{
+    val parts = line.split(",")
+    return if(parts.size == 5){
+        TradeRecord(parts[0].toInt(), parts[1], parts[2], parts[3].toDouble(), parts[4].toDouble())
+    }else null
+}
