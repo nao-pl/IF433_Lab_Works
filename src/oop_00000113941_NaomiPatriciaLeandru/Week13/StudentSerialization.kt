7dfcc8ad139682fdb728cb5cc66d188c702c3cc7
Nao <naomileandru@gmail.com>
2026-05-18 19:49:04 +0700

    "week13: implement toCsv and fromCsv logic"

--- src/oop_00000113941_NaomiPatriciaLeandru/Week13/StudentSerialization.kt
+++ src/oop_00000113941_NaomiPatriciaLeandru/Week13/StudentSerialization.kt
@@ -7,3 +7,10 @@ data class Student(
     val age: Int, 
     val gpa: Double
 )
+
+fun Student.toCsv(): String = "$name,$age,$gpa"
+
+fun fromCsv(line: String): Student{
+    val parts = line.split(",")
+    return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
+}
